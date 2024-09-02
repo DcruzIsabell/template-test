5c68936e29a67f33f56c6492e41cfdf889fd5762
Isabell D'cruz <152013681+DcruzIsabell@users.noreply.github.com>
2024-09-02 15:17:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./webmanager/**/*.{html,js}"],
   theme: {
     extend: {
       colors:
